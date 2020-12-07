61e1209b06e73f8cb984d1a8fe51c643e7870974
Dyneteve <dyneteve@hentaios.com>
2020-12-08 03:53:49 +0900

    vendor: audio: Update from RQ1A.201205.011
    Change-Id: Ic83b9a69a9c5e99b3cdfd1a22df6f4f20ca7d403

--- config/audio.mk
+++ config/audio.mk
@@ -63,7 +63,9 @@ PRODUCT_COPY_FILES += \
     vendor/pixelbuilds/audio/ui/Unlock.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/Unlock.ogg \
     vendor/pixelbuilds/audio/ui/VideoRecord.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/VideoRecord.ogg \
     vendor/pixelbuilds/audio/ui/VideoStop.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/VideoStop.ogg \
-    vendor/pixelbuilds/audio/ui/WirelessChargingStarted.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/WirelessChargingStarted.ogg
+    vendor/pixelbuilds/audio/ui/WirelessChargingStarted.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/WirelessChargingStarted.ogg \
+    vendor/pixelbuilds/audio/ui/reverse_charging_end.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/reverse_charging_end.ogg \
+    vendor/pixelbuilds/audio/ui/reverse_charging_start.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui/reverse_charging_start.ogg
 
 # Change default sounds
 PRODUCT_PRODUCT_PROPERTIES += \
