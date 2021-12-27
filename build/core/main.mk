a0670ab4f0f518abd0b2f603fc23863b00c59ab8
PixelBoot <bootingpixels@gmail.com>
2021-12-27 08:14:20 -0800

    vendor: inherit pixelbuilds versions
    Change-Id: I21edbb2f82bdad98f36fb3fdd541e004d8bbe703

--- build/core/main.mk
+++ build/core/main.mk
@@ -1,2 +1,2 @@
-# Include LineageOS versions
-include $(TOPDIR)vendor/lineage/build/core/main_version.mk
+# Include PixelBuilds versions
+include $(TOPDIR)vendor/pixelbuilds/build/core/main_version.mk
--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -1,25 +1,43 @@
 # Build fingerprint
 ifneq ($(BUILD_FINGERPRINT),)
+BUILD_NUMBER_CUSTOM := $(shell date -u +%H%M)
+CUSTOM_DEVICE ?= $(TARGET_DEVICE)
+ifneq ($(filter OFFICIAL,$(CUSTOM_BUILD_TYPE)),)
+BUILD_SIGNATURE_KEYS := release-keys
+else
+BUILD_SIGNATURE_KEYS := test-keys
+endif
+BUILD_FINGERPRINT := $(PRODUCT_BRAND)/$(CUSTOM_DEVICE)/$(CUSTOM_DEVICE):$(PLATFORM_VERSION)/$(BUILD_ID)/$(BUILD_NUMBER_CUSTOM):$(TARGET_BUILD_VARIANT)/$(BUILD_SIGNATURE_KEYS)
+endif
 ADDITIONAL_BUILD_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
+
+# AOSP recovery flashing
+ifeq ($(TARGET_USES_AOSP_RECOVERY),true)
+ADDITIONAL_BUILD_PROPERTIES += \
+    persist.sys.recovery_update=true
 endif
 
-# LineageOS System Version
-ADDITIONAL_BUILD_PROPERTIES += \
-    ro.lineage.version=$(LINEAGE_VERSION) \
-    ro.lineage.releasetype=$(LINEAGE_BUILDTYPE) \
-    ro.lineage.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
-    ro.modversion=$(LINEAGE_VERSION) \
-    ro.lineagelegal.url=https://lineageos.org/legal
+# PixelBuilds Build Time
+CUSTOM_DATE_YEAR := $(shell date -u +%Y)
+CUSTOM_DATE_MONTH := $(shell date -u +%m)
+CUSTOM_DATE_DAY := $(shell date -u +%d)
+CUSTOM_DATE_HOUR := $(shell date -u +%H)
+CUSTOM_DATE_MINUTE := $(shell date -u +%M)
+CUSTOM_BUILD_DATE_UTC := $(shell date -d '$(CUSTOM_DATE_YEAR)-$(CUSTOM_DATE_MONTH)-$(CUSTOM_DATE_DAY) $(CUSTOM_DATE_HOUR):$(CUSTOM_DATE_MINUTE) UTC' +%s)
+CUSTOM_BUILD_DATE := $(CUSTOM_DATE_YEAR)$(CUSTOM_DATE_MONTH)$(CUSTOM_DATE_DAY)-$(CUSTOM_DATE_HOUR)$(CUSTOM_DATE_MINUTE)
 
-# LineageOS Platform Display Version
-ADDITIONAL_BUILD_PROPERTIES += \
-    ro.lineage.display.version=$(LINEAGE_DISPLAY_VERSION)
+# PixelBuilds Platform Display Version
+CUSTOM_BUILD_TYPE ?= UNOFFICIAL
+CUSTOM_PLATFORM_VERSION := 11.0
+TARGET_PRODUCT_SHORT := $(subst aosp_,,$(CUSTOM_BUILD))
+CUSTOM_VERSION := PixelBuilds_$(CUSTOM_BUILD)-$(CUSTOM_PLATFORM_VERSION)-$(CUSTOM_BUILD_DATE)-$(CUSTOM_BUILD_TYPE)
+CUSTOM_VERSION_PROP := retina
 
-# LineageOS Platform SDK Version
+# PixelBuilds System Version
 ADDITIONAL_BUILD_PROPERTIES += \
-    ro.lineage.build.version.plat.sdk=$(LINEAGE_PLATFORM_SDK_VERSION)
-
-# LineageOS Platform Internal Version
-ADDITIONAL_BUILD_PROPERTIES += \
-    ro.lineage.build.version.plat.rev=$(LINEAGE_PLATFORM_REV)
+    ro.pixelbuilds.version=$(CUSTOM_VERSION_PROP) \
+    ro.pixelbuilds.version.display=$(CUSTOM_VERSION) \
+    ro.pixelbuilds.build_date=$(CUSTOM_BUILD_DATE) \
+    ro.pixelbuilds.build_date_utc=$(CUSTOM_BUILD_DATE_UTC) \
+    ro.pixelbuilds.build_type=$(CUSTOM_BUILD_TYPE)
